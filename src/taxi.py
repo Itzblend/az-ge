98ef8e6cd24d01f565d663963ec562cc99e7ff01
Lauri <huhta.lauri@gmail.com>
2023-08-22 18:00:24 +0300

    Self hosted runner AZ suite storage

--- src/taxi.py
+++ src/taxi.py
@@ -129,7 +129,6 @@ def create_taxi_config():
 
     # Datasource
     datasource_config = get_azure_datastore_config()
-
     context.test_yaml_config(yaml.dump(datasource_config))
     context.add_or_update_datasource(**datasource_config)
     # Checkpoint
